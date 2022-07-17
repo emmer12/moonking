46ce2a9651d3fb39b610c744873b760769adb0d3
makerzy <botassammzy@gmail.com>
2022-07-17 02:37:36 -0400

    update'

---
+++ src/generated/graphql-frontend.ts
@@ -0,0 +1,205 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions = {} as const;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createUser: User;
+  updateUser: User;
+};
+
+
+export type MutationCreateUserArgs = {
+  account: Scalars['String'];
+};
+
+
+export type MutationUpdateUserArgs = {
+  account: Scalars['String'];
+  claim_hash: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  getUser: User;
+};
+
+
+export type QueryGetUserArgs = {
+  account: Scalars['String'];
+};
+
+export type User = {
+  __typename?: 'User';
+  account: Scalars['String'];
+  amount: Scalars['String'];
+  claim_hash?: Maybe<Scalars['String']>;
+  claim_signature?: Maybe<Scalars['String']>;
+  claimed: Scalars['Boolean'];
+  claimed_at?: Maybe<Scalars['String']>;
+  eligible: Scalars['Boolean'];
+  id: Scalars['ID'];
+  nonce?: Maybe<Scalars['String']>;
+  token?: Maybe<Scalars['String']>;
+};
+
+export type CreateUserMutationVariables = Exact<{
+  account: Scalars['String'];
+}>;
+
+
+export type CreateUserMutation = { __typename?: 'Mutation', createUser: { __typename?: 'User', id: string, claim_hash?: string | null, claim_signature?: string | null, claimed: boolean, amount: string, claimed_at?: string | null, eligible: boolean, account: string } };
+
+export type GetUserQueryVariables = Exact<{
+  account: Scalars['String'];
+}>;
+
+
+export type GetUserQuery = { __typename?: 'Query', getUser: { __typename?: 'User', id: string, claim_hash?: string | null, claim_signature?: string | null, token?: string | null, claimed: boolean, amount: string, claimed_at?: string | null, eligible: boolean, account: string } };
+
+export type UpdateUserMutationVariables = Exact<{
+  account: Scalars['String'];
+  claim_hash: Scalars['String'];
+}>;
+
+
+export type UpdateUserMutation = { __typename?: 'Mutation', updateUser: { __typename?: 'User', id: string, claim_hash?: string | null, claim_signature?: string | null, claimed: boolean, amount: string, claimed_at?: string | null, eligible: boolean, account: string } };
+
+
+export const CreateUserDocument = gql`
+    mutation CreateUser($account: String!) {
+  createUser(account: $account) {
+    id
+    claim_hash
+    claim_signature
+    claimed
+    amount
+    claimed_at
+    eligible
+    account
+  }
+}
+    `;
+export type CreateUserMutationFn = Apollo.MutationFunction<CreateUserMutation, CreateUserMutationVariables>;
+
+/**
+ * __useCreateUserMutation__
+ *
+ * To run a mutation, you first call `useCreateUserMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateUserMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createUserMutation, { data, loading, error }] = useCreateUserMutation({
+ *   variables: {
+ *      account: // value for 'account'
+ *   },
+ * });
+ */
+export function useCreateUserMutation(baseOptions?: Apollo.MutationHookOptions<CreateUserMutation, CreateUserMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateUserMutation, CreateUserMutationVariables>(CreateUserDocument, options);
+      }
+export type CreateUserMutationHookResult = ReturnType<typeof useCreateUserMutation>;
+export type CreateUserMutationResult = Apollo.MutationResult<CreateUserMutation>;
+export type CreateUserMutationOptions = Apollo.BaseMutationOptions<CreateUserMutation, CreateUserMutationVariables>;
+export const GetUserDocument = gql`
+    query GetUser($account: String!) {
+  getUser(account: $account) {
+    id
+    claim_hash
+    claim_signature
+    token
+    claimed
+    amount
+    claimed_at
+    eligible
+    account
+  }
+}
+    `;
+
+/**
+ * __useGetUserQuery__
+ *
+ * To run a query within a React component, call `useGetUserQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetUserQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetUserQuery({
+ *   variables: {
+ *      account: // value for 'account'
+ *   },
+ * });
+ */
+export function useGetUserQuery(baseOptions: Apollo.QueryHookOptions<GetUserQuery, GetUserQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetUserQuery, GetUserQueryVariables>(GetUserDocument, options);
+      }
+export function useGetUserLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetUserQuery, GetUserQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetUserQuery, GetUserQueryVariables>(GetUserDocument, options);
+        }
+export type GetUserQueryHookResult = ReturnType<typeof useGetUserQuery>;
+export type GetUserLazyQueryHookResult = ReturnType<typeof useGetUserLazyQuery>;
+export type GetUserQueryResult = Apollo.QueryResult<GetUserQuery, GetUserQueryVariables>;
+export const UpdateUserDocument = gql`
+    mutation UpdateUser($account: String!, $claim_hash: String!) {
+  updateUser(account: $account, claim_hash: $claim_hash) {
+    id
+    claim_hash
+    claim_signature
+    claimed
+    amount
+    claimed_at
+    eligible
+    account
+  }
+}
+    `;
+export type UpdateUserMutationFn = Apollo.MutationFunction<UpdateUserMutation, UpdateUserMutationVariables>;
+
+/**
+ * __useUpdateUserMutation__
+ *
+ * To run a mutation, you first call `useUpdateUserMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useUpdateUserMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [updateUserMutation, { data, loading, error }] = useUpdateUserMutation({
+ *   variables: {
+ *      account: // value for 'account'
+ *      claim_hash: // value for 'claim_hash'
+ *   },
+ * });
+ */
+export function useUpdateUserMutation(baseOptions?: Apollo.MutationHookOptions<UpdateUserMutation, UpdateUserMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<UpdateUserMutation, UpdateUserMutationVariables>(UpdateUserDocument, options);
+      }
+export type UpdateUserMutationHookResult = ReturnType<typeof useUpdateUserMutation>;
+export type UpdateUserMutationResult = Apollo.MutationResult<UpdateUserMutation>;
+export type UpdateUserMutationOptions = Apollo.BaseMutationOptions<UpdateUserMutation, UpdateUserMutationVariables>;
